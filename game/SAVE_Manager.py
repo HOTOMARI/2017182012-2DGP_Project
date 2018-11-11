86b253995d89ce561a38e4b4604ff0d5b699e3cd
hogumin_MyPC <hojiny9923@gmail.com>
2018-11-11 11:58:51 +0900

    save_manager
    스킬 정보까지 저장 완료

--- game/SAVE_Manager.py
+++ game/SAVE_Manager.py
@@ -9,12 +9,17 @@ def Save_game():
     f.write(str(GPD.money) + '\n')
     for i in range(0,4):
         f.write(str(GPD.items[i].NUM)+'\n')
-        pass
+
     # 캐릭터 정보
     for i in range(0,4):
         f.write(str(GPD.players[i].HP) + '\n' + str(GPD.players[i].MAX_HP) + '\n')
         f.write(str(GPD.players[i].MP) + '\n' + str(GPD.players[i].MAX_MP) + '\n')
-    pass
+        f.write(str(GPD.players[i].LEVEL) + '\n' + str(GPD.players[i].EXP) + '\n')
+        f.write(str(GPD.players[i].MAX_EXP) + '\n' + str(GPD.players[i].ATK) + '\n')
+        f.write(str(GPD.players[i].DEF) + '\n' + str(GPD.players[i].AP) + '\n')
+
+        for s in range(0,4):
+            f.write(str(GPD.players[i].skill[s].POWER) + '\n')
 
 
 def Load_game():
