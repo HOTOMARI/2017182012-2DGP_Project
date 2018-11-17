4c59ab23635c68986f696a9a6babf6c91f719edc
hogumin_MyPC <hojiny9923@gmail.com>
2018-11-17 16:59:25 +0900

    Update BaseEffect.py
    스킬 효과 출력

--- game/BaseEffect.py
+++ game/BaseEffect.py
@@ -71,7 +71,8 @@ class Effect():
                                       192, 200, 300, 400, 400)
 
         elif self.id == 25:
-            pass
+            GPD.Assassinate.image.clip_draw(0 + int(self.frame % 5) * 192, int(192 * 2) - int(self.frame / 5) * 192, 192,
+                                          192, 200, 410 - 100 * party_num, 128, 128)
 
         elif self.id == 26:
             pass
