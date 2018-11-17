e9bd6cdf9cba45f3910c68f3cead0ed0e540227a
hogumin_MyPC <hojiny9923@gmail.com>
2018-11-17 16:58:20 +0900

    Update BaseEffect.py
    스킬 이펙트 출력

--- game/BaseEffect.py
+++ game/BaseEffect.py
@@ -67,7 +67,8 @@ class Effect():
                                          192, 200, 410 - 100 * party_num, 128, 128)
 
         elif self.id == 24:
-            pass
+            GPD.DeathBlossom.image.clip_draw(0 + int(self.frame % 5) * 192, int(192 * 5) - int(self.frame / 5) * 192, 192,
+                                      192, 200, 300, 400, 400)
 
         elif self.id == 25:
             pass
