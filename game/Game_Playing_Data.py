702192bc2e48acac20cacfe00be891d5d2c40faf
hogumin_MyPC <hojiny9923@gmail.com>
2018-11-17 11:04:38 +0900

    provoke
    effect change

--- game/Game_Playing_Data.py
+++ game/Game_Playing_Data.py
@@ -2,7 +2,7 @@ from pico2d import*
 import overworld_charactrer
 
 # 구별 id : 프레임 수
-skill_MAXframe ={0:3, 1:19, 2:29, 11:5, 12:6, 13:24, 14:16, 15:6, 16:19, 17:19, 18:22}
+skill_MAXframe ={0:3, 1:19, 2:29, 11:15, 12:6, 13:24, 14:16, 15:6, 16:19, 17:19, 18:22}
 
 players=[[]for i in range(4)]
 monsters=[[]for i in range(3)]
