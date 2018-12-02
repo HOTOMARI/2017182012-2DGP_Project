19cb1edd7a67bd8f536ee64eef81aee2e8475579
hogumin_MyPC <hojiny9923@gmail.com>
2018-12-02 16:48:29 +0900

    Update s_Stone.py
    스킬로그 수정

--- game/s_Stone.py
+++ game/s_Stone.py
@@ -11,9 +11,9 @@ class Stone(Skill):
         super(Stone, self).__init__('스톤', Skill_Data['Stone']['ID'], Skill_Data['Stone']['COST'], Skill_Data['Stone']['POWER'], Skill_Data['Stone']['UPGRADE'])
 
     def activate(self, my_index, target_index):
-        GPD.Battlelog.append(self.name + '의 스톤')
+        GPD.Battlelog.append(GPD.players[my_index].name + '의 스톤')
         GPD.monsters[target_index].HP -= int((self.POWER+GPD.players[my_index].ATK))
-        GPD.Battlelog.append(self.name + '이 ' + GPD.monsters[target_index].name + '에게 ' + str(
+        GPD.Battlelog.append(GPD.players[my_index].name + '가 ' + GPD.monsters[target_index].name + '에게 ' + str(
             int((self.POWER + GPD.players[my_index].ATK))) + '피해')
         GPD.monsters[target_index].hate[my_index] += self.POWER
 
